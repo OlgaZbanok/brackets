d98f863a298aaeea71244c774810fb93ab57e735
Olga Zbanok <olga@zbanok.com>
2019-09-30 22:35:34 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let confmap  = new Map (bracketsConfig);
+  let mas=[];
+  for (let s of str)
+  {
+    if (confmap.has(s))
+    {
+        if ((confmap.get(s) == s) && (mas.length!=0))
+        {
+          let samebracket = mas.pop();
+          if (samebracket != s)
+          {
+            mas.push(samebracket);
+            mas.push(s);
+          }
+        }
+        else 
+          mas.push(s);
+    }
+    else 
+    {
+      if ( (mas.length == 0) || (confmap.get(mas.pop()) != s))
+        return false;
+    }
+  }
+  if (mas.length == 0) 
+    return true; 
+  else
+    return false;
 }
